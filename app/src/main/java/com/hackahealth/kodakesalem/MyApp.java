69c632aac106c4d7038271fb463d5b2a9e59ba08
siavash <siavash.abdoli@gmail.com>
2016-05-19 12:53:48 +0430

    activity login added

--- app/src/main/java/com/hackahealth/kodakesalem/MyApp.java
+++ app/src/main/java/com/hackahealth/kodakesalem/MyApp.java
@@ -3,6 +3,8 @@ package com.hackahealth.kodakesalem;
 import android.app.Application;
 import android.content.Context;
 
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+
 /**
  * Created by siavash on 5/18/16.
  */
@@ -13,5 +15,13 @@ public class MyApp extends Application{
     public void onCreate() {
         super.onCreate();
         applicationContext=this;
+        initFont();
     }
+    private void initFont() {
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath(getString(R.string.iran_sans_regular))
+                .setFontAttrId(R.attr.fontPath)
+                .build());
+    }
+
 }
